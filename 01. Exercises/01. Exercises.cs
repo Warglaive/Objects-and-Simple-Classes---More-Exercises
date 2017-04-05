d26c9a201a5baca914c7555ae51f9437e9d86ffb
Warglaive <warglaivegm@gmail.com>
2017-04-05 18:24:06 +0300

    Update 01. Exercises.cs

--- 01. Exercises/01. Exercises.cs
+++ 01. Exercises/01. Exercises.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,10 +17,7 @@ namespace _01.Exercises
     {
        public static void Main()
         {
-<<<<<<< HEAD
             var input = Console.ReadLine();
-
-
             var result = new List<Exercise>();
             while (input!="go go go")
             {
@@ -29,10 +26,7 @@ namespace _01.Exercises
                 var courseName = token[1];
                 var judgeContestLink = token[2];
                 var problems = token.Skip(3).ToList();
-
-
                 var newExercise = new Exercise();
-
                 newExercise.Topic = topic;
                 newExercise.CourseName = courseName;
                 newExercise.JudgeContestLink = judgeContestLink;
@@ -50,37 +44,6 @@ namespace _01.Exercises
                 {
                     Console.WriteLine("{0}. {1}", counter, problem);
                     counter++;
-=======
-            var exercises = new List<Exercise>();
-            string inputLine = Console.ReadLine();
-            while (inputLine != "go go go")
-            {
-                var inputParams = inputLine.Split(new[] { ' ', '-', '>', ',' }, StringSplitOptions.RemoveEmptyEntries);
-                string topic = inputParams[0];
-                string courseName = inputParams[1];
-                string JudgeContestLink = inputParams[2];
-                List<string> problems = inputParams.Skip(3).ToList();
-
-                var newExercise = new Exercise();
-                newExercise.Topic = topic;
-                newExercise.CourseName = courseName;
-                newExercise.JudgeContestLink = JudgeContestLink;
-                newExercise.Problems = problems;
-                exercises.Add(newExercise);
-                inputLine = Console.ReadLine();
-            }
-            foreach (var exercise in exercises)
-            {
-                Console.WriteLine($"Exercises: {exercise.Topic}");
-                Console.WriteLine($"Problems for exercises and homework for the \"{exercise.CourseName}\" course @ SoftUni.");
-                Console.WriteLine($"Check your solutions here {exercise.JudgeContestLink}");
-
-                var count = 1;
-                foreach (var problem in exercise.Problems)
-                {
-                    Console.WriteLine("{0}. {1}", count, problem);
-                    count++;
->>>>>>> 658d3e1db967a0c0019df25d83cbe6c8ffb6cce2
                 }
             }
         }
