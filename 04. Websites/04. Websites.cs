dcb56a33a93f0e987cf9f9bd37b0e7bbf131d2c9
Warglaive <warglaivegm@gmail.com>
2017-04-06 22:12:07 +0300

    04. Websites PC

---
+++ 04. Websites/04. Websites.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Websites
+{
+    public class Website
+    {
+        public string Host { get; set; }
+        public string Domain { get; set; }
+        public List<string> Queries { get; set; }
+    }
+   public class Program
+    {
+       public static void Main()
+        {
+            var input = Console.ReadLine();
+            var result = new List<Website>();
+            while (input != "end") 
+            {
+                var token = input.Split(new[] { ' ', '|',',' },StringSplitOptions.RemoveEmptyEntries);
+                var host = token[0];
+                var domain = token[1];
+                var queries = token.Skip(2).ToList();
+                var newWebsite = new Website();
+                newWebsite.Host = host;
+                newWebsite.Domain = domain;
+                newWebsite.Queries = queries;
+                result.Add(newWebsite);
+                input = Console.ReadLine();
+            }
+            foreach (var item in result)
+            {
+                if (item.Queries.Count > 1)
+                {
+                    Console.Write($"https://www.{item.Host}.{item.Domain}/");
+                    for (int i = 0; i < 1; i++)
+                    {
+                        Console.Write($"query?=[{item.Queries[i]}]");
+                        for (int j = i + 1; j < item.Queries.Count; j++)
+                        {
+                            Console.Write($"&[{item.Queries[j]}]");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"https://www.{item.Host}.{item.Domain}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
